083d7526e11cdd6914405b0a3ed9512d0f6a9617
AlexanderDuya <alexduyaconnect@gmail.com>
2024-08-04 14:20:03 +0100

    blackjack + personal portfolio

---
+++ blackjack/Player.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+class Player {	  	   	      	   	       	     	    	
+    private String name; // player 1 or 2	  	   	      	   	       	     	    	
+    private ArrayList<Card> cards = new ArrayList<>(); // ArrayList to store cards	  	   	      	   	       	     	    	
+    private boolean done = false; // boolean for whether they are finished their turn	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+    //name methods	   & card methods	  	   	      	   	       	     	    	
+    public Player(String name) {	  	   	      	   	       	     	    	
+        this.name = name;	  	   	      	   	       	     	    	
+    }	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+    public String getName() {	  	   	      	   	       	     	    	
+        return name;	  	   	      	   	       	     	    	
+    }	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+    public ArrayList<Card> getCards() {	  	   	      	   	       	     	    	
+        return cards;	  	   	      	   	       	     	    	
+    }	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+    // representing the card as a string	  	   	      	   	       	     	    	
+    public String getCardsAsString() {	  	   	      	   	       	     	    	
+        String result = "";	  	   	      	   	       	     	    	
+        for (Card card : cards) {	  	   	      	   	       	     	    	
+            result += card.toString() + ", ";	  	   	      	   	       	     	    	
+        }	  	   	      	   	       	     	    	
+        return result;	  	   	      	   	       	     	    	
+    }	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+    // checking if player is finished their turn	  	   	      	   	       	     	    	
+    public boolean isDone() {	  	   	      	   	       	     	    	
+        return done;	  	   	      	   	       	     	    	
+    }	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+    //setting their turn status	  	   	      	   	       	     	    	
+    public void setDone(boolean done) {	  	   	      	   	       	     	    	
+        this.done = done;	  	   	      	   	       	     	    	
+    }	  	   	      	   	       	     	    	
+	  	   	      	   	       	     	    	
+    // method to add card	  	   	      	   	       	     	    	
+    public void addCard(Card c) {	  	   	      	   	       	     	    	
+        cards.add(c);	  	   	      	   	       	     	    	
+    }	  	   	      	   	       	     	    	
+}
